ec787257dafd3ce166a5cdb3fcecd047f1d4744a
wilson <wilson.veterano@zarph.com>
2021-05-27 11:51:40 +0100

    .

--- src/index.js
+++ src/index.js
@@ -1,21 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from "react";
+import ReactDOM from "react-dom";
 //component file
-import TodoContainer from './functionBased/components/TodoContainer'
-import "./functionBased/App.css"
-import { BrowserRouter } from "react-router-dom"
+import TodoContainer from "./functionBased/components/TodoContainer";
+import "./functionBased/App.css";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
-<<<<<<< HEAD
   <BrowserRouter basename={process.env.PUBLIC_URL}>
     <TodoContainer />
   </BrowserRouter>,
   document.getElementById("root")
 );
-=======
-    <BrowserRouter>
-        <TodoContainer />
-    </BrowserRouter>,
-    document.getElementById("root")
-)
->>>>>>> 9760fd0e00d56132dc55679f49b671c95ff2b6ec
